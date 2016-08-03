1d3d6aa862988a579b22cd17ab11ab0147f4ec02
merubin <michael@rubinsworld.com>
2016-08-03 17:11:52 -0400

    Unit 8 file repository completed

---
+++ js/main.js
@@ -0,0 +1,16 @@
+console.log("JS file is connected to HTML! Woo! Woo!")
+
+/////////////////////////////////////////////////////////////////////////////
+  // Declare variables
+  /////////////////////////////////////////////////////////////////////////////
+
+
+var cardOne = "Queen", 
+	cardTwo = "Queen",
+	cardThree = "King", 
+	cardFour = "King";
+
+
+console.log(" cardOne == cardTwo ", cardOne == cardTwo);
+console.log(" cardOne == cardFour", cardOne == cardFour);
+
